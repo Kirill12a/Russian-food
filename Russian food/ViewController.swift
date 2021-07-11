51c674510890a2303d249bd352ba40433de2dcfb
Kirill12a <kirilldrozd12@gmail.com>
2021-07-11 18:21:10 +0300

    Initial Commit

---
+++ Russian food/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Russian food
+//
+//  Created by Kirill Drozdov on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
