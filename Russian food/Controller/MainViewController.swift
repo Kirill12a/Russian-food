95897e3c174d448388c1781035d728a42fae32d5
Kirill12a <kirilldrozd12@gmail.com>
2021-07-12 11:19:17 +0300

    New

--- Russian food/Controller/MainViewController.swift
+++ Russian food/Controller/MainViewController.swift
@@ -6,39 +6,12 @@
 //
 
 import UIKit
-import BATabBarController
+import Foundation
 
-
-class MainViewController: UIViewController, BATabBarControllerDelegate {
+class MainViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
-        let vc1 = UIViewController()
-        let vc2 = UIViewController()
-        let vc3 = UIViewController()
-        
-        let tabBarItem  = BATabBarItem(image: UIImage(named: "intro1")!, selectedImage: UIImage(named: "intro1")!)
-        let tabBarItem2 = BATabBarItem(image: UIImage(named: "intro4")!, selectedImage: UIImage(named: "intro4")!)
-        let tabBarItem3 = BATabBarItem(image: UIImage(named: "intro5")!, selectedImage: UIImage(named: "intro5")!)
-        
-        
-        let baTabBarController = BATabBarController()
-        baTabBarController.viewControllers = [vc1, vc2, vc3]
-        baTabBarController.tabBarItems = [tabBarItem, tabBarItem2, tabBarItem3]
-        baTabBarController.delegate = self
-        self.view.addSubview(baTabBarController.view)
-        print("fvsfd")
-        
-        
-        
-    }
-    
-    func tabBarController(_ tabBarController: BATabBarController, didSelect: UIViewController) {
-        
-       
-        
     }
     
 }
