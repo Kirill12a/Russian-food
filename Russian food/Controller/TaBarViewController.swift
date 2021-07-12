6f9e9d43e03595f1bb510741def4342eb3c3dfb8
Kirill12a <kirilldrozd12@gmail.com>
2021-07-12 12:46:42 +0300

    начал делать scrollview

---
+++ Russian food/Controller/TaBarViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TaBarViewController.swift
+//  Russian food
+//
+//  Created by Kirill Drozdov on 12.07.2021.
+//
+
+import UIKit
+
+class TaBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // по умолчанию vc
+        self.selectedIndex = 1
+        
+        // цвет
+//        tabBar.backgroundColor = .systemGray
+        
+    }
+    
+}
